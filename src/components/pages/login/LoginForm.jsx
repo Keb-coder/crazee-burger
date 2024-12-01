7250831e997619eeb307dc04c9e20b18a61e54d3
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-12-01 15:05:46 +0100

    💄 F03: LOGIN > LoginForm> input > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BsPersonCircle } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
@@ -20,15 +21,18 @@ export default function LoginForm() {
 			<h1>Bienvenue chez nous !</h1>
 			<hr />
 			<h2>Connectez-vous</h2>
-			<input
-				id="connexion"
-				value={inputValue}
-				onChange={handleCHange}
-				type="text"
-				name="Idendifiant"
-				placeholder="Entrez votre prénom"
-				required
-			/>
+              <div className="input-with-icon">
+			  <BsPersonCircle className="icon" />
+				<input
+					id="connexion"
+					value={inputValue}
+					onChange={handleCHange}
+					type="text"
+					name="Idendifiant"
+					placeholder="Entrez votre prénom"
+					required
+				/> 
+              </div>
 
 			<button type="submit" id="connexionButton">
 				Accéder à mon espace
@@ -64,5 +68,31 @@ h2{
 	margin: 20px 10px 10px;
 	color: white;
 	font-size: 36px;
+}
+
+.input-with-icon{
+	background-color: #fff;
+	border-radius: 5px;
+	display: flex;
+	align-items: center;
+	padding:18px 24px;
+	margin: 18px 0;
+}
+
+.icon {
+	color: yellow;
+	font-size: 15px;
+	margin-right: 8px;
+	color: #93a2b1;;
+}
+
+input{
+	border: none;
+	font-size: 15px;
+}
+
+&::placeholder{
+	background: white;
+	color:lightgrey;
 }
 `;
