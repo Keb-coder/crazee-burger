f48838bd543cb2f7f58e2f4ba1cd3b23388eed38
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-12-01 10:26:26 +0100

    💄 F03 : LoginPage > add layout

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,34 +1,43 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function LoginForm() {
-  const [inputValue, setInputValue] = useState("");
-  const navigate = useNavigate();
+	const [inputValue, setInputValue] = useState("");
+	const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setInputValue("");
-    navigate(`order/${inputValue}`);
-  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		setInputValue("");
+		navigate(`order/${inputValue}`);
+	};
 
-  const handleCHange = (event) => {
-    setInputValue(event.target.value);
-  };
-  return (
-    <div>
-      <h1>Bienvenue chez nous !</h1>
-      <h3>Connectez-vous</h3>
-      <form action="submit" onSubmit={handleSubmit}>
-        <input
-          value={inputValue}
-          onChange={handleCHange}
-          type="text"
-          name="Idendifiant"
-          placeholder="Entrez votre prénom"
-          required
-        />
-        <button>Accéder à mon espace</button>
-      </form>
-    </div>
-  );
+	const handleCHange = (event) => {
+		setInputValue(event.target.value);
+	};
+	return (
+		<LoginFormStyled action="submit" onSubmit={handleSubmit}>
+			<h1>Bienvenue chez nous !</h1>
+			<h3>Connectez-vous</h3>
+			<input
+				id="connexion"
+				value={inputValue}
+				onChange={handleCHange}
+				type="text"
+				name="Idendifiant"
+				placeholder="Entrez votre prénom"
+				required
+			/>
+
+			<button type="submit" id="connexionButton">
+				Accéder à mon espace
+			</button>
+		</LoginFormStyled>
+	);
 }
+
+const LoginFormStyled = styled.div`
+background: blue;
+display: flex;
+flex-direction:column;
+`;
--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,9 +1,24 @@
+import styled from "styled-components";
 import LoginForm from "./LoginForm";
+import burgerBackground from "../../../assets/images/burger-background.jpg";
+import Logo from "../../reusable-ui/Logo";
 
 export default function LoginPage() {
-  return (
-    <div>
-      <LoginForm />
-    </div>
-  );
+	return (
+		<LoginPageStyled>
+			<Logo />
+			<LoginForm />
+		</LoginPageStyled>
+	);
 }
+
+const LoginPageStyled = styled.div` 
+  box-sizing: border-box;
+  background-image: url(${burgerBackground});
+  height: 100vh;
+  background: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction:column;
+`;
