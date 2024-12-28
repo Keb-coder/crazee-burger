215478baa438bebd164a4deab94e2cdec7cc8bf6
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-12-28 10:22:35 +0100

    ✨ F04: ORDER > Navbar > trigger reload on click

--- src/components/pages/login/order/Navbar.jsx
+++ src/components/pages/login/order/Navbar.jsx
@@ -2,11 +2,12 @@ import styled from "styled-components";
 import NavbarRIghtSide from "./NavbarRIghtSide";
 import Logo from "../../../reusable-ui/Logo";
 import { theme } from "../../../../theme";
+import { refreshPage } from "../../../../utils/window";
 
 export default function Navbar({ username }) {
 	return (
 		<NavBarStyled>
-			<Logo />
+			<Logo classname={"logo-order-page"} onClick={() => refreshPage()} />
 			<NavbarRIghtSide username={username} />
 		</NavBarStyled>
 	);
@@ -21,4 +22,7 @@ padding: 0 20px;
 border-top-right-radius: ${theme.borderRadius.extraRound};
 border-top-left-radius: ${theme.borderRadius.extraRound};
 
+.logo-order-page{
+	cursor:pointer;
+}
 `;
--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -3,9 +3,9 @@ import styled from "styled-components";
 import { theme } from "../../theme";
 import logo from "../../assets/images/logo-orange.png";
 
-export default function Logo({ classname }) {
+export default function Logo({ classname, onClick }) {
 	return (
-		<LogoStyled className={classname}>
+		<LogoStyled className={classname} onClick={onClick}>
 			<h1>CRAZEE</h1>
 			<img src={logo} alt="logo-crazee-burger" />
 			<h1>BURGER</h1>
