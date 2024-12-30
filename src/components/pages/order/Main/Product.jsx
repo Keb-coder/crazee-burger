3edc8301e109a561e01d4e0d4eb8eb219ec94ff9
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-12-30 22:43:03 +0100

    💄 F05: ORDER > Product > center image and description

--- src/components/pages/order/Main/Product.jsx
+++ src/components/pages/order/Main/Product.jsx
@@ -8,8 +8,8 @@ export default function Product({title, imageSource, price }) {
             <img src={imageSource} alt={title} />
           </div>
           <div className="info-text">
-            <div className="description">
-              <div className="title">{title}</div>
+            <div className="title">{title}</div>
+              <div className="description">
               <div className="price">{price}</div>
               <button className="add-button">Ajouter</button>
             </div>
@@ -24,24 +24,31 @@ const ProductStyled = styled.div`
   
   .product{
   background: blue;
-  width: 240px;
-  height: 330px;
+  width: 200px;
+  height: 300px;
+  display: grid;
+  grid-template-rows: 65% 1fr;
+  padding: 20px;
+  margin-top: 30px;
+
 
   .image{
-    width: 100px;
+    width: 100%;
     height:auto;
-    border: 1px solid fuchsia;
+    margin-top: 30px;
+    border: 1px solid red;
+
 
   img{
     width:100%;
     height:100%;
-    
+    object-fit:contain;
   }
 
 }
+.info-text{
+    border: 1px solid red;
+}
 
-  .description{
-    border: 1px solid fuchsia;
-  }
 }  
 `;
