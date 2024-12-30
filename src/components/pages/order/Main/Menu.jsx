5163d9a0277ad7559b0739dfe555e28015b9aa7d
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-12-30 16:06:19 +0100

    🍱 F05: Add assets

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function Menu() {
+
+const [menu, setMenu] =  useState(fakeMenu)
+
   return (
     <MenuStyled className="menu">Menu</MenuStyled>
   )
