d9072fafde57570317ceb8251df43ca95b86130b
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2025-01-04 11:11:04 +0100

    💄 F05: ORDER > MENU > Add final style

--- src/components/pages/reusable-ui/PrimaryButton.jsx
+++ src/components/pages/reusable-ui/PrimaryButton.jsx
@@ -1,13 +1,13 @@
-import styled from "styled-components";
-import { theme } from "../../../theme";
+import styled from "styled-components"
+import { theme } from "../../../theme"
 
-export default function PrimaryButton({ label, Icon }) {
-	return (
-		<PrimaryButtonStyled>
-			<span>{label}</span>
-			{Icon && Icon}
-		</PrimaryButtonStyled>
-	);
+export default function PrimaryButton({ label, Icon, className }) {
+  return (
+    <PrimaryButtonStyled className={className}>
+      <span>{label}</span>
+      {Icon && Icon}
+    </PrimaryButtonStyled>
+  )
 }
 
 const PrimaryButtonStyled = styled.button`
@@ -16,7 +16,7 @@ const PrimaryButtonStyled = styled.button`
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  position: relative; 
+  position: relative;
   white-space: nowrap; 
   text-decoration: none; 
   line-height: 1;
@@ -25,25 +25,39 @@ const PrimaryButtonStyled = styled.button`
   border-radius: 5px;
   font-size: 15px;
   font-weight: 800;
-  color: ${theme.colors.white};
-  background-color: ${theme.colors.primary};
+  color: white;
+  background-color:${theme.colors.primary} ;
   border: 1px solid ${theme.colors.primary};
 
-  &:hover:not(:disabled) {
-    background-color: ${theme.colors.white};
+  &:hover {
     color: ${theme.colors.primary};
+    background-color: ${theme.colors.white};
     border: 1px solid ${theme.colors.primary};
     transition: all 200ms ease-out;
   }
-
   &:active {
-    color: white;
     background-color: ${theme.colors.primary};
-    border: 1px solid ${theme.colors.primary};
+    color: ${theme.colors.white};
   }
 
-  &:disabled {
-    opacity: 0.6;
+  &.is-disabled {
+    opacity: 50%;
     cursor: not-allowed;
+    z-index: 2;
   }
-`;
+
+  &.with-focus {
+    border: 1px solid white;
+    background-color: ${theme.colors.white};
+    color: ${theme.colors.primary};
+    :hover {
+      color: ${theme.colors.white};
+      background-color: ${theme.colors.primary};
+      border: 1px solid ${theme.colors.white};
+    }
+    :active {
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.primary};
+    }
+  }
+`
