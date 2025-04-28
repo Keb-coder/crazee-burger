a15465161987f900b389a4af4dd6ef1e316181bf
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2025-04-28 16:13:11 +0200

    💄 F07: MAIN > Add layout with Basket

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -4,27 +4,44 @@ import Menu from "./Menu";
 
 export default function Main() {
 	return (
-  <MainStyled className="main">
-    {/* <div className="basket">basket</div> */}
-    <Menu/>
-</MainStyled>
-  )
+		<MainStyled className="main">
+			{/* <div className="basket">basket</div> */}
+			<div className="menu-and-admin">
+				<Menu />
+				<div className="admin">admin</div>
+			</div>
+		</MainStyled>
+	);
 }
 
 const MainStyled = styled.div`
+  border: 3px solid green;
   background: ${theme.colors.background_white};
-  flex: 1;  
+  height: calc(95vh-10vh);
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   display: grid;
-  grid-template-columns: 1fr;
+  grid-template-columns:1fr;
 
   overflow-y: scroll;
 
-  /* .basket{ */
-    /* background: pink; */
-  /* } */
+  /* .basket{
+    background: pink;
+  } */
 
-`; 
+  .menu-and-admin{
+    position:relative; 
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    border-bottom-right-radius: ${theme.borderRadius.extraRound};
+  .admin{
+    background: red;
+    position: absolute;
+    height:200px;
+    bottom:0;
+    left:0;
+    right:0;
+  }
+}
+`;
